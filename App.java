3611561c4d8d9b215264166c5a5bd47d44e8d671
student <isl1304@yandex.ru>
2017-12-06 19:53:51 +0400

    gener

--- App.java
+++ App.java
@@ -3,5 +3,9 @@ package gener;
 public class App {
     public static void main(String[] args) {
 
+        Test1<Integer,Integer> test1 = new Test1<>(1,"2");
+        Test1<String,Integer> test2 = new Test1<>("a",2);
+        Test1 test3 = new Test1(1,2);
+
     }
 }
---
+++ Test1.java
@@ -0,0 +1,14 @@
+package gener;
+
+public class Test1<T1,T2> {
+    T1 aa;
+    T2 bb;
+
+    public Test1(T1 aa, T2 bb) {
+        this.aa = aa;
+        this.bb = bb;
+    }
+
+
+
+}
